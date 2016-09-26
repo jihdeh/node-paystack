57c46a663eca922a9874b736c931753a9d573866
Jihdeh <jide.b.tade@gmail.com>
2016-09-26 15:51:07 +0100

    refactor to singly method calls

--- src/__tests__/initialize-test.js
+++ src/__tests__/initialize-test.js
@@ -2,12 +2,13 @@ import Paystack from "../";
 
 const callPaystack = new Paystack("Bearer sk_test_008b0a2fa73be91009ed49733e5e61d0c9d95b86");
 describe("Initialize paystack transaction", () => {
-  it("should make request to Initialize transaction", async (done) => {
+  it("should make request to Initialize transaction", (done) => {
     const initializeData = {
       amount: 500,
       email: "jide.b.tade@gmail.com"
     }
-    const response = callPaystack.transaction().initialize(initializeData);
+    const response = callPaystack.transaction.initialize(initializeData);
+    console.log(response, "0-")
     done();
   });
 
@@ -16,8 +17,8 @@ describe("Initialize paystack transaction", () => {
       amount: 500,
       email: "jide.b.tade@gmail.com"
     }
-    const response = callPaystack.transaction().initialize(initializeData).verify();
-    console.log(response);
+    // const response = callPaystack.transaction().initialize(initializeData).verify();
+    // console.log(response);
     // done();
   });
 })
--- src/config.js
+++ src/config.js
@@ -1,31 +1,31 @@
 import request from "./requests";
-import {get} from "lodash";
+import {get } from "lodash";
 let storeSecret;
+
 function Paystack(key) {
   storeSecret = key;
   return this;
-  
 }
 
-Paystack.prototype.transaction = function(first_argument) {
-  return {
-    initialize: function(initArgs) {
-      const response = request(storeSecret, initArgs, "initialize");
-      this.init = response.data;
-      return this;
-    },
-    
-    verify: function(reference) {
-      if(this.init) {
-        const response = request(storeSecret, get(this.init, "data.data.reference"), "verify");
-        this.verfied = response;
-      } else {
-        console.log("here")
-        const response = request(storeSecret, reference, "verify");
-        this.verfied = response;
-      }
-      return this;
-    } 
+Paystack.prototype.transaction = {
+  initialize: async(initArgs) => {
+    const response = await request(storeSecret, initArgs);
+    this.init = response.data;
+    return this;
+  },
+
+  verify: function(reference) {
+    if (this.init) {
+      const waitResponse = (async function() {
+        const response = await request(storeSecret, get(this.init, "data.data.reference"), "verify");
+        return response;
+      })();
+      this.verfied = waitResponse;
+    } else {
+      const response = request(storeSecret, reference, "verify");
+      this.verfied = response;
+    }
+    return this;
   }
 };
 
